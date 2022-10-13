e0d66f91348048e354547eb64090fb6d3031cfa0
yq.W <yuqi.wang.b@17zuoye.com>
2022-10-13 19:34:07 +0800

    临时修改

--- test_demo/path_data01/api01.py
+++ test_demo/path_data01/api01.py
@@ -10,6 +10,7 @@
 
 from fastapi import FastAPI
 from enum import Enum
+import uvicorn
 
 app = FastAPI()
 
@@ -32,20 +33,51 @@ async def read_user_id(user_id: str):
 
 
 # 枚举 预设值
-
-
 class ModelName(str, Enum):
     alexnet = "alexnet"
     resnet = "resnet"
     lenet = "lenet"
 
 
+# 声明路径参数
 @app.get("/models/{model_name}")
 async def get_model(model_name: ModelName):
     if model_name is ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
+        return {"model_name": model_name, "message": "传参="+model_name}
     if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
+        return {"model_name": model_name, "message": "参数="+model_name}
+    return {"model_name": model_name, "message": "参数="+model_name}
 
-    return {"model_name": model_name, "message": "Have some residuals"}
+
+# 路径转换器
+
+@app.get("/files/{files_path: path}")
+async def get_path(files_path: str):
+    return {"files_path": files_path}
+
+
+
+# 查询参数
+
+test_db = [{"name": "a", "name": "b", "name": "c", "name": "d"}]
+@app.get("/get_data/")
+async def get_data(skip: int = 0, limit: int = 1):
+    return test_db[skip: skip + limit]
+
+# 可选参数
+@app.get()
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    uvicorn.run("api01:app", reload=True)
